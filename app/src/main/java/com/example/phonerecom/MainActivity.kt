0f3b7669bda370ca1c2bdbd74e12cd0d1f5530ef
salinillas <4b23salinas@gmail.com>
2024-11-23 03:04:58 +0100

    commit

--- app/src/main/java/com/example/phonerecom/MainActivity.kt
+++ app/src/main/java/com/example/phonerecom/MainActivity.kt
@@ -5,10 +5,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -17,7 +14,6 @@ import com.example.phonerecom.ui.theme.PhoneRecomTheme
 class MainActivity : ComponentActivity() {
     private val viewModel: LoginViewModel by viewModels { LoginViewModelFactory(DatabaseHelper(this)) }
     private val phoneViewModel: PhoneViewModel by viewModels { PhoneViewModelFactory(DatabaseHelper(this))}
-    private val recommendedPhone: Phone? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
